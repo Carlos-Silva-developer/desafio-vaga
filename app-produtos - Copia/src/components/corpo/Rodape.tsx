904dfdc6d365f1aef11161a8e624d11025cc0288
Carlos Darci Melo da Silva <devcarlosdms@gmail.com>
2023-11-03 12:46:30 -0300

    adicionado collection do postman

--- app-produtos - Copia/src/components/corpo/Rodape.tsx
+++
@@ -1,7 +0,0 @@
-export default function Rodape() {
-    return (
-        <footer className="flex justify-center my-5">
-            <span>My Costumer - All rights not reserved - 2023</span>
-        </footer>
-    )
-}
